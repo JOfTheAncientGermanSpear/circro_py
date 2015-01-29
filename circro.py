ea4e249cdef7eca2e3fa05b56e246a959d31c6f8
joftheancientgermanspear <joftheancientgermanspear@users.noreply.github.com>
2015-01-29 17:15:40 -0500

    create data structure to represent nodes and edges

--- circro.py
+++ circro.py
@@ -1,7 +1,8 @@
-from pandas import read_csv
+from pandas import read_csv, concat, Series, DataFrame
+import numpy as np
 
-def namedInputsToDict(**inputs):
-    return inputs
+def inputsToDict(**kwords):
+    return kwords
 
 class InputError(Exception):
     def __init__(self, msg):
@@ -9,12 +10,39 @@ class InputError(Exception):
     def __str__(self):
         return repr(self.msg)
 
+def read_node_file(filename):
+    df = read_csv(filename, header=None)
+    return concat([df[0], df[1]], keys=['left', 'right']).to_frame()
+
+
 def make_circro(labels = None, sizes = None, colors = None, edge_matrix = None):
-    inputs = namedInputsToDict(labels = labels, sizes = sizes, 
+    data_filenames = inputsToDict(labels = labels, sizes = sizes, 
             colors = colors, edge_matrix = edge_matrix)
 
-    if not len(inputs):
-        raise InputError("at least one of the method inputs must be set")
+    file_keys = ['labels', 'sizes', 'colors', 'edge_matrix']
 
-    return {i: (read_csv(v, header=None) if isinstance(v, str) else v)
-        for i,v in inputs.items() if v}
+    res = {}
+
+    if not any(f in data_filenames for f in file_keys):
+        raise InputError("at least one of {} inputs must be set".format(file_keys))
+
+    node_dfs = {k: read_node_file(f) 
+            for k,f in data_filenames.items() if f and 'edge' not in k}
+
+    for k, df in node_dfs.items():
+        df.columns = [k[:-1]] 
+
+
+    res['nodes'] = concat(list(node_dfs.values()), axis = 1)
+
+    if edge_matrix:
+        res['edges'] = read_csv(edge_matrix, header=None)
+
+    if labels:
+        labels = res['nodes']['label']
+
+        if 'edges' in res:
+            res['edges'].columns = labels.values
+            res['edges'].index = labels.values
+
+    return res
