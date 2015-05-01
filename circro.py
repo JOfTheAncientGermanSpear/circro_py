cacc5307534523efafbabdf47904e8dfc0b5225b
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-05-01 14:19:56 -0400

    remove tabs with 4 spaces

--- circro.py
+++ circro.py
@@ -22,82 +22,82 @@ def _inputs_to_dict(**kwords):
     return kwords
 
 def _scale_matrix(mat):
-	"""
-	>>> import numpy as np
-	>>> x = np.array([[1, 2], [3, 5]])
-	>>> _scale_matrix(x)
-	array([[ 0.  ,  0.25],
-	       [ 0.5 ,  1.  ]])
-	"""
-	mx = mat.max()
-	mn = mat.min()
-	rg = mx - mn
-	return (mat - mn)/rg
+    """
+    >>> import numpy as np
+    >>> x = np.array([[1, 2], [3, 5]])
+    >>> _scale_matrix(x)
+    array([[ 0.  ,  0.25],
+           [ 0.5 ,  1.  ]])
+    """
+    mx = mat.max()
+    mn = mat.min()
+    rg = mx - mn
+    return (mat - mn)/rg
 
 
 def _lazy_df(fn, df):
-	"""
-	>>> import numpy as np
-	>>> import pandas as pd
-	>>> df = pd.DataFrame(np.array([[1, 2], [3, 5]]))
-	>>> fn = lambda i: [i - 1, i, i + 1]
-	>>> lz_df = _lazy_df(fn, df)
-	>>> lz_df.iloc[0][0]()
-	[0, 1, 2]
-	>>> lz_df.iloc[0][1]()
-	[1, 2, 3]
-	>>> lz_df.iloc[1][0]()
-	[2, 3, 4]
-	>>> lz_df.iloc[1][1]()
-	[4, 5, 6]
-	"""
-	lazy_fn = lambda i: lambda: fn(i)
-	return df.applymap(lazy_fn)
+    """
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> df = pd.DataFrame(np.array([[1, 2], [3, 5]]))
+    >>> fn = lambda i: [i - 1, i, i + 1]
+    >>> lz_df = _lazy_df(fn, df)
+    >>> lz_df.iloc[0][0]()
+    [0, 1, 2]
+    >>> lz_df.iloc[0][1]()
+    [1, 2, 3]
+    >>> lz_df.iloc[1][0]()
+    [2, 3, 4]
+    >>> lz_df.iloc[1][1]()
+    [4, 5, 6]
+    """
+    lazy_fn = lambda i: lambda: fn(i)
+    return df.applymap(lazy_fn)
 
 
 def _read_node_file(filename, node_type):
-	"""
-	>>> x = 'test_data/labels.csv'
-	>>> (df, cols) = _read_node_file(x, 'labels')
-	>>> df
-	        labels
-	left  0    BA1
-	      1    BA2
-	      2    BA3
-	right 0    BA4
-	      1    BA5
-	      2    BA6
-	>>> cols
-	['Regions L', 'Regions R']
-	"""
-	data = pd.read_csv(filename)
-	(left, right) = data.columns
-	df = pd.concat([data[left], data[right]], keys=['left', 'right']).to_frame()
-	df.columns = [node_type]
-	return (df, [left, right])
+    """
+    >>> x = 'test_data/labels.csv'
+    >>> (df, cols) = _read_node_file(x, 'labels')
+    >>> df
+            labels
+    left  0    BA1
+          1    BA2
+          2    BA3
+    right 0    BA4
+          1    BA5
+          2    BA6
+    >>> cols
+    ['Regions L', 'Regions R']
+    """
+    data = pd.read_csv(filename)
+    (left, right) = data.columns
+    df = pd.concat([data[left], data[right]], keys=['left', 'right']).to_frame()
+    df.columns = [node_type]
+    return (df, [left, right])
 
 def _create_nodes_df(filename_dict):
-	"""
-	>>> x = {'labels': 'test_data/labels.csv', 'sizes': 'test_data/sizes.csv'}
-	>>> (df, cols) = _create_nodes_df(x)
-	>>> df
-	        labels  sizes
-	left  0	   BA1    1.0
-	      1    BA2    2.0
-	      2    BA3    3.0
-	right 0    BA4    1.5
-	      1    BA5    2.5
-	      2    BA6    3.5
-	>>> cols
-	{'labels': ['Regions L', 'Regions R'], 'sizes': ['Sizes L', 'Sizes R']}
-	"""
-	node_file_keys = ['labels', 'sizes', 'colors']
-	dfs_cols = [_read_node_file(f, k) for k,f in filename_dict.items() 
-			if f and k in node_file_keys]
-	dfs = [df_col[0] for df_col in dfs_cols]
-	cols = {df_col[0].columns[0]: df_col[1] for df_col in dfs_cols}
-	df = pd.concat(dfs, axis = 1)
-	return (df, cols)
+    """
+    >>> x = {'labels': 'test_data/labels.csv', 'sizes': 'test_data/sizes.csv'}
+    >>> (df, cols) = _create_nodes_df(x)
+    >>> df
+            labels  sizes
+    left  0    BA1    1.0
+          1    BA2    2.0
+          2    BA3    3.0
+    right 0    BA4    1.5
+          1    BA5    2.5
+          2    BA6    3.5
+    >>> cols
+    {'labels': ['Regions L', 'Regions R'], 'sizes': ['Sizes L', 'Sizes R']}
+    """
+    node_file_keys = ['labels', 'sizes', 'colors']
+    dfs_cols = [_read_node_file(f, k) for k,f in filename_dict.items() 
+            if f and k in node_file_keys]
+    dfs = [df_col[0] for df_col in dfs_cols]
+    cols = {df_col[0].columns[0]: df_col[1] for df_col in dfs_cols}
+    df = pd.concat(dfs, axis = 1)
+    return (df, cols)
 
 
 def _create_edges_df(edge_file, nodes):
@@ -106,17 +106,17 @@ def _create_edges_df(edge_file, nodes):
     >>> left = pd.Series(['BA1', 'BA2', 'BA3'])
     >>> right = pd.Series(['BA4', 'BA5', 'BA6'])
     >>> nodes = pd.concat([left, right], keys = ['left', 'right']).to_frame()
-	>>> nodes.columns = ['labels']
-	>>> edge_file = 'test_data/edge_matrix.csv'
-	>>> df = _create_edges_df(edge_file, nodes)
-	>>> df
-	     BA1  BA2  BA3  BA4  BA5  BA6
-	BA1  0.0  1.2  1.3  1.4  1.5  1.6
-	BA2  1.2  0.0  2.3  0.0  0.0  0.0
-	BA3  1.3  2.3  0.0  0.0  0.0  3.6
-	BA4  1.4  0.0  0.0  0.0  4.5  0.0
-	BA5  1.5  0.0  0.0  4.5  0.0  0.0
-	BA6  1.6  0.0  3.6  0.0  0.0  0.0
+    >>> nodes.columns = ['labels']
+    >>> edge_file = 'test_data/edge_matrix.csv'
+    >>> df = _create_edges_df(edge_file, nodes)
+    >>> df
+         BA1  BA2  BA3  BA4  BA5  BA6
+    BA1  0.0  1.2  1.3  1.4  1.5  1.6
+    BA2  1.2  0.0  2.3  0.0  0.0  0.0
+    BA3  1.3  2.3  0.0  0.0  0.0  3.6
+    BA4  1.4  0.0  0.0  0.0  4.5  0.0
+    BA5  1.5  0.0  0.0  4.5  0.0  0.0
+    BA6  1.6  0.0  3.6  0.0  0.0  0.0
     """
     edges = pd.read_csv(edge_file, header=None)
     if 'labels' in nodes:
@@ -126,7 +126,7 @@ def _create_edges_df(edge_file, nodes):
     return edges
 
 def _raise_input_error(inputs):
-	raise InputError("at least one of {} inputs must be set".format(inputs))
+    raise InputError("at least one of {} inputs must be set".format(inputs))
 
 
 def make_circro(labels = None, sizes = None, colors = None, edge_matrix = None,
@@ -155,32 +155,32 @@ def make_circro(labels = None, sizes = None, colors = None, edge_matrix = None,
     return res
 
 def make_circro_from_dir(src_dir, inner_r = 1.0, start_radian = 0.0, edge_threshold = .5, node_cm = 'jet', edge_cm = 'jet'):
-	"""
-	>>> src_dir = 'data' #data has files for labels, colors, sizes, edge_matrix 
-	>>> my_circ_dir = make_circro_from_dir(src_dir)
-	>>> import os
-	>>> prep = lambda(l): os.path.join(src_dir, l + '.csv')
-	>>> my_circ = make_circro(labels = prep('labels'), sizes = prep('sizes'), colors = prep('colors'), edge_matrix = prep('edge_matrix'))
-	>>> from pandas.util.testing import assert_frame_equal
-	>>> assert_frame_equal(my_circ['nodes'], my_circ_dir['nodes'])
-	>>> assert_frame_equal(my_circ['edges'], my_circ_dir['edges'])
-	"""
-	file_keys = {'labels', 'colors', 'sizes', 'edge_matrix'}
+    """
+    >>> src_dir = 'data' #data has files for labels, colors, sizes, edge_matrix 
+    >>> my_circ_dir = make_circro_from_dir(src_dir)
+    >>> import os
+    >>> prep = lambda(l): os.path.join(src_dir, l + '.csv')
+    >>> my_circ = make_circro(labels = prep('labels'), sizes = prep('sizes'), colors = prep('colors'), edge_matrix = prep('edge_matrix'))
+    >>> from pandas.util.testing import assert_frame_equal
+    >>> assert_frame_equal(my_circ['nodes'], my_circ_dir['nodes'])
+    >>> assert_frame_equal(my_circ['edges'], my_circ_dir['edges'])
+    """
+    file_keys = {'labels', 'colors', 'sizes', 'edge_matrix'}
 
-	def add_file_input(acc, i):
-		f = os.path.join(src_dir, i + '.csv')
-		acc[i] = f if os.path.isfile(f) else None
-		return acc
-	
-	inputs = reduce(add_file_input, file_keys, dict())
+    def add_file_input(acc, i):
+        f = os.path.join(src_dir, i + '.csv')
+        acc[i] = f if os.path.isfile(f) else None
+        return acc
+    
+    inputs = reduce(add_file_input, file_keys, dict())
 
-	if all(inputs[i] == None for i in file_keys):
-		_raise_input_error(file_keys)
+    if all(inputs[i] == None for i in file_keys):
+        _raise_input_error(file_keys)
 
-	inputs.update(_inputs_to_dict(inner_r = inner_r, start_radian = start_radian,
-		edge_threshold = edge_threshold, node_cm = node_cm, edge_cm = edge_cm))
+    inputs.update(_inputs_to_dict(inner_r = inner_r, start_radian = start_radian,
+        edge_threshold = edge_threshold, node_cm = node_cm, edge_cm = edge_cm))
 
-	return make_circro(**inputs)
+    return make_circro(**inputs)
 
 
 def _calculate_radial_arc(start_radian, end_radian, radius): 
@@ -189,7 +189,7 @@ def _calculate_radial_arc(start_radian, end_radian, radius):
     theta_gap_orig = end_radian - start_radian
 
     theta_gap =  theta_gap_orig if theta_gap_orig < np.pi else 2*np.pi - theta_gap_orig
-	
+    
     theta_mid = np.pi/2
     theta_left = theta_mid - theta_gap/2
     theta_right = theta_mid + theta_gap/2
@@ -216,48 +216,48 @@ def _calculate_radial_arc(start_radian, end_radian, radius):
     return (rs * radius, thetas)
 
 def _plot_info(circ):
-	num_nodes = len(circ['nodes']) if 'nodes' in circ else len(circ['edges'])
-	rad_per_node = 2 * np.pi/num_nodes
+    num_nodes = len(circ['nodes']) if 'nodes' in circ else len(circ['edges'])
+    rad_per_node = 2 * np.pi/num_nodes
 
-	info = {}
-	nodes = pd.DataFrame()
+    info = {}
+    nodes = pd.DataFrame()
 
-	#get the index & main data fram circ
-	nodes['label'] = circ['nodes']['labels'] if 'labels' in circ['nodes'] else circ['nodes'].index.labels[1]
-	nodes['size'] = circ['nodes']['sizes'] if 'sizes' in circ['nodes'] else 1
+    #get the index & main data fram circ
+    nodes['label'] = circ['nodes']['labels'] if 'labels' in circ['nodes'] else circ['nodes'].index.labels[1]
+    nodes['size'] = circ['nodes']['sizes'] if 'sizes' in circ['nodes'] else 1
 
-	nodes['width'] = rad_per_node
-	nodes['width'].right = rad_per_node * -1
+    nodes['width'] = rad_per_node
+    nodes['width'].right = rad_per_node * -1
 
-	node_cm = getattr(cm, circ['node_cm'])
+    node_cm = getattr(cm, circ['node_cm'])
 
-	info['node_colors'] = node_cm(circ['nodes']['colors'] if 'colors' in circ['nodes'] else 1.0)
+    info['node_colors'] = node_cm(circ['nodes']['colors'] if 'colors' in circ['nodes'] else 1.0)
 
-	start_radian = circ['start_radian']
-	nodes['theta'] = nodes['width'] * nodes.index.labels[1]
+    start_radian = circ['start_radian']
+    nodes['theta'] = nodes['width'] * nodes.index.labels[1]
 
-	nodes['label_loc'] = nodes['theta'] * 180/np.pi
-	nodes['label_loc'].right = 360 + nodes['label_loc'].right
+    nodes['label_loc'] = nodes['theta'] * 180/np.pi
+    nodes['label_loc'].right = 360 + nodes['label_loc'].right
 
-	deg_per_node = np.rad2deg(rad_per_node)
-	nodes['label_loc'].left = nodes['label_loc'].left + deg_per_node/2
-	nodes['label_loc'].right = nodes['label_loc'].right - deg_per_node/2
+    deg_per_node = np.rad2deg(rad_per_node)
+    nodes['label_loc'].left = nodes['label_loc'].left + deg_per_node/2
+    nodes['label_loc'].right = nodes['label_loc'].right - deg_per_node/2
 
-	start_deg = np.rad2deg(start_radian)
-	nodes['label_loc'] = nodes['label_loc'] + start_deg
-	nodes['theta'] = nodes['theta'] + start_radian
+    start_deg = np.rad2deg(start_radian)
+    nodes['label_loc'] = nodes['label_loc'] + start_deg
+    nodes['theta'] = nodes['theta'] + start_radian
 
-	nodes['label_loc'] = np.mod(nodes['label_loc'], 360.0)
-	nodes['theta'] = np.mod(nodes['theta'], 2*np.pi)
+    nodes['label_loc'] = np.mod(nodes['label_loc'], 360.0)
+    nodes['theta'] = np.mod(nodes['theta'], 2*np.pi)
 
-	info['nodes'] = nodes
+    info['nodes'] = nodes
 
-	if 'edges' in circ:
-		scaled_edges = _scale_matrix(circ['edges'])
-		edge_cm = getattr(cm, circ['edge_cm'])
-		info['edge_colors'] = _lazy_df(edge_cm, scaled_edges) 
+    if 'edges' in circ:
+        scaled_edges = _scale_matrix(circ['edges'])
+        edge_cm = getattr(cm, circ['edge_cm'])
+        info['edge_colors'] = _lazy_df(edge_cm, scaled_edges) 
 
-	return info
+    return info
 
 def plot_circro(my_circ, draw = True):
     info = _plot_info(my_circ)
@@ -290,13 +290,13 @@ def plot_circro(my_circ, draw = True):
                     clr = info['edge_colors'][start_label][end_label]()
                     ax.plot(thetas, radii, color = clr)
 
-	if draw:
-	    plt.show()
+    if draw:
+        plt.show()
 
 def plot_circros(my_circs):
-	for c in my_circs:
-		plot_circro(c, draw = False)
-	plt.show()
+    for c in my_circs:
+        plot_circro(c, draw = False)
+    plt.show()
 
 
 if __name__ == "__main__":
